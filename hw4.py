9bc1bb3a443cc72a794c47fd15a348f6df66dac3
mattplon <91156717+mattplon@users.noreply.github.com>
2021-09-30 23:56:00 -0400

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -28,7 +28,9 @@ class Customer:
     # Submit_order takes a cashier, a stall and an amount as parameters, 
     # it deducts the amount from the customer’s wallet and calls the receive_payment method on the cashier object
     def submit_order(self, cashier, stall, amount): 
-        pass
+        self.wallet -= amount
+        cashier = Cashier(self.name)
+        cashier.receive_payment(stall, amount)
 
     # The __str__ method prints the customer's information.    
     def __str__(self):
